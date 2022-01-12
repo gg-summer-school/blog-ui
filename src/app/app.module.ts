968b8fa1ce345505b81dfc7c855ae9df0af4ab74
unknown <Hotou-Morelle>
2022-01-12 14:18:48 +0100

    downloads and project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { GuardsComponent } from './auth/guards/guards.component';
+import { InterceptorsComponent } from './auth/interceptors/interceptors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    GuardsComponent,
+    InterceptorsComponent
   ],
   imports: [
     BrowserModule,
