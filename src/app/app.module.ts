67da41b7a6beaa5ddd6c763d089d89338664fbee
fionaanye <fiona.anye@go-groups.net>
2022-01-13 14:53:13 +0200

    completed the design for the user and publisher for under admin, then, the request part from user that wants to upgrade or be a publisher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuardsComponent } from './auth/guards/guards.component';
+import { InterceptorsComponent } from './auth/interceptors/interceptors.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PublisherAdminComponent } from './pages/publisher-admin/publisher-admin.component';
+import { RequestsComponent } from './pages/requests/requests.component';
+import { UserAdminComponent } from './pages/user-admin/user-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuardsComponent,
+    InterceptorsComponent,
+    DashboardComponent,
+    PublisherAdminComponent,
+    RequestsComponent,
+    UserAdminComponent
   ],
   imports: [
     BrowserModule,
