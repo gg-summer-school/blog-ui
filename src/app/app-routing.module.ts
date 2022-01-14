4ba70d05b2dd8c52a407a4b38bb055f102c65a44
fiona-max <anyefiona@gmail.com>
2022-01-14 13:39:14 +0100

    completing the article-detail-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import {YourArticlesComponent} from './pages/your-articles/your-articles.component';
 import {UserProfileComponent} from './pages/user-profile/user-profile.component';
+import {ArticleDetailPageComponent} from './pages/article-detail-page/article-detail-page.component';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: LandingPageComponent
   },
   {
     path: 'login',
@@ -25,10 +27,12 @@ const routes: Routes = [
   {
     path: 'user-profile',
     component: UserProfileComponent
+  },
+  {
+    path: 'articles-detail',
+    component: ArticleDetailPageComponent
   }
-
-];
-
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
