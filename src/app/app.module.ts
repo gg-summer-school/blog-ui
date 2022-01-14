c67dae3c18e6e7c7e3368c12c57ad16c55805363
fiona-max <anyefiona@gmail.com>
2022-01-14 08:36:05 +0100

    Detail article page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { YourArticlesComponent } from './pages/your-articles/your-articles.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { ArticleDetailPageComponent } from './pages/article-detail-page/article-detail-page.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { UserProfileComponent } from './pages/user-profile/user-profile.componen
     DashboardComponent,
     SignupComponent,
     YourArticlesComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ArticleDetailPageComponent
   ],
   imports: [
     BrowserModule,
