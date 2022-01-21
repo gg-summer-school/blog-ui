990eafee7ba847d5c3a6d4e37363293138393fc1
Morelle-Olivia <hotoumorelle@gmail.com>
2022-01-21 15:52:18 +0200

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,13 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://192.168.8.106:8000/api/public/auth',
-  baseUrlPro: 'http://192.168.8.106:8000/api/protected/',
-  baseUrlPub: 'http://192.168.8.106:8000/api/public/',
+  // baseUrl: 'http://192.168.8.106:8000/api/public/auth',
+  // baseUrlPro: 'http://192.168.8.106:8000/api/protected/',
+  // baseUrlPub: 'http://192.168.8.106:8000/api/public/',
+
+  baseUrl: 'http://localhost:8000/api/public/auth',
+  baseUrlPro: 'http://localhost:8000/api/protected/',
+  baseUrlPub: 'http://localhost:8000/api/public/',
 };
 
 /*
