6182abedaa11a36dc5c6e171832acf16506b316f
Morelle-Olivia <hotoumorelle@gmail.com>
2022-01-28 10:01:36 +0200

    Done with the translation of view-transactions components

--- src/app/pages/view-transactions/view-transactions.component.ts
+++ src/app/pages/view-transactions/view-transactions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AdminPagesService} from "../../services/admin-services/admin-pages.service";
 import {Transactions} from "../../model/admin";
 import {TokenStorageService} from "../../services/token-storage.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-view-transactions',
@@ -13,7 +14,13 @@ export class ViewTransactionsComponent implements OnInit {
   transactions: Transactions[] = [];
   userId: string = '';
 
-  constructor(private adminPagesService: AdminPagesService, private tokenStorageService: TokenStorageService) { }
+  constructor(private adminPagesService: AdminPagesService, private tokenStorageService: TokenStorageService,
+              public translate: TranslateService) { }
+
+  switchLang(lang: string) {
+    console.log(lang)
+    this.translate.use(lang);
+  }
 
   ngOnInit(): void {
     this.userId = this.tokenStorageService.getUser().id;
