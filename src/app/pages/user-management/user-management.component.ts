abcf4fef36d3ae81e7cae903898db744bc2e62fd
Morelle-Olivia <hotoumorelle@gmail.com>
2022-02-08 18:31:37 +0200

    Done with one issue

--- src/app/pages/user-management/user-management.component.ts
+++ src/app/pages/user-management/user-management.component.ts
@@ -4,23 +4,38 @@ import {Admin, roleDTO, RolePayload} from "../../model/admin";
 import {NotificationMessageService} from "../../services/Notification/notification-message.service";
 import {NotificationType} from "../../model/NotificationMessage";
 import {NgxSpinnerService} from "ngx-spinner";
+import {TokenStorageService} from "../../services/token-storage.service";
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss']
 })
+
+// export enum RolePayload {
+//   ROLE_ADMIN= "ROLE_ADMIN",
+//   ROLE_PUBLISHER ="ROLE_PUBLISHER",
+//   ROLE_READER="ROLE_READER"
+// }
+
 export class UserManagementComponent implements OnInit {
+  user2: any[] = [];
+
   number!: number;
-  isApproved: boolean = true;
   publishers: Admin[] = [];
   readers: Admin[] = [];
   allUsers: Admin[] = [];
+  RolePayload: RolePayload[] = [];
 
   constructor(private adminPagesService: AdminPagesService,  private notificationService:NotificationMessageService,
-              private  spinnerService: NgxSpinnerService) { }
+              private  spinnerService: NgxSpinnerService, private tokenStore: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.user2 = [
+      'ROLE_ADMIN',
+      'ROLE_PUBLISHER',
+      'ROLE_READER'
+    ]
     this.displayAllUsers();
   }
 
@@ -55,6 +70,7 @@ export class UserManagementComponent implements OnInit {
     const role : roleDTO = {
       role : (<any>RolePayload)[event.target.value]
     }
+    console.log(event.target.value);
     this.adminPagesService.removeRole(user_id, role).subscribe((res: any) => {
   this.spinnerService.hide()
     }, (error: any) => {
