d46928143d666da1b8587c75a9205d32f31fc6bd
Chi-Claudette <chiclaudette2000@gmail.com>
2022-02-16 16:15:17 +0100

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,10 +9,10 @@ export const environment = {
   // baseUrlPro: 'http://localhost:8000/api/protected/',
   // baseUrlPub: 'http://localhost:8000/api/public/',
 
- 
-  baseUrl: 'http://192.168.1.23:8000/api/public/auth',
-  baseUrlPro: 'http://192.168.1.23:8000/api/protected/',
-  baseUrlPub: 'http://192.168.1.23:8000/api/public/',
- 
+
+  baseUrl: 'http://192.168.8.104:8000/api/public/auth',
+  baseUrlPro: 'http://192.168.8.104:8000/api/protected/',
+  baseUrlPub: 'http://192.168.8.104:8000/api/public/',
+
 
 };
