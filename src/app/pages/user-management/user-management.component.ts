582c6ba6c15919de39afe503657048439a657219
fiona-max <anyefiona@gmail.com>
2022-02-16 17:07:35 +0100

    adjusted the mobile responsiveness

--- src/app/pages/user-management/user-management.component.ts
+++ src/app/pages/user-management/user-management.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {AdminPagesService} from "../../services/admin-services/admin-pages.service";
-import {Admin, roleDTO, RolePayload} from "../../model/admin";
-import {NotificationMessageService} from "../../services/Notification/notification-message.service";
-import {NotificationType} from "../../model/NotificationMessage";
-import {NgxSpinnerService} from "ngx-spinner";
-import {TokenStorageService} from "../../services/token-storage.service";
+import {AdminPagesService} from '../../services/admin-services/admin-pages.service';
+import {Admin, roleDTO, RolePayload} from '../../model/admin';
+import {NotificationMessageService} from '../../services/Notification/notification-message.service';
+import {NotificationType} from '../../model/NotificationMessage';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {TokenStorageService} from '../../services/token-storage.service';
 
 @Component({
   selector: 'app-user-management',
@@ -24,7 +24,7 @@ export class UserManagementComponent implements OnInit {
   allUsers: Admin[] = [];
   rolePayload: Admin[] = [];
 
-  constructor(private adminPagesService: AdminPagesService,  private notificationService:NotificationMessageService,
+  constructor(private adminPagesService: AdminPagesService,  private notificationService: NotificationMessageService,
               private  spinnerService: NgxSpinnerService, private tokenStore: TokenStorageService) { }
 
   ngOnInit(): void {
@@ -32,11 +32,12 @@ export class UserManagementComponent implements OnInit {
       'ROLE_ADMIN',
       'ROLE_PUBLISHER',
       'ROLE_READER'
-    ]
+    ];
     this.displayAllUsers();
   }
 
-  getUserRoles(userId : string) {
+  // tslint:disable-next-line:typedef
+  getUserRoles(userId: string) {
     const user = this.allUsers.find((user) => user.id === userId);
     const role = [...this.user2];
     const tes: string[] = [];
@@ -44,13 +45,13 @@ export class UserManagementComponent implements OnInit {
     this.userRoles.push(user.role);
 
     // @ts-ignore
-    const test2 = user.role.map(function (obj) {
+    const test2 = user.role.map(function(obj) {
       return obj.role;
     });
 
     const filteredArray = role.filter(value => !test2.includes(value));
-    for(let filter of filteredArray){
-      this.enableRole = (filter)
+    for (const filter of filteredArray){
+      this.enableRole = (filter);
     }
 
     // for(let i = 0; i<=filteredArray.length; i++) {
@@ -61,44 +62,44 @@ export class UserManagementComponent implements OnInit {
   }
 
   displayAllUsers() {
-    this.spinnerService.show()
+    this.spinnerService.show();
     this.adminPagesService.getUsers()
       .subscribe( res =>
       {
         this.allUsers = res;
         this.rolePayload = this.allUsers;
-        this.number= this.allUsers.length;
-        this.spinnerService.hide()
-      })
+        this.number = this.allUsers.length;
+        this.spinnerService.hide();
+      });
   }
 
 
 
-  addRoleToUser(user_id: string, event:any) {
-    this.spinnerService.show()
-    const role : roleDTO = {
-      role : (<any>RolePayload)[event]
-    }
+  addRoleToUser(user_id: string, event: any) {
+    this.spinnerService.show();
+    const role: roleDTO = {
+      role : (RolePayload as any)[event]
+    };
     this.adminPagesService.appendRole(user_id, role).subscribe((res: any) => {
-  this.spinnerService.hide()
+  this.spinnerService.hide();
     }, (error: any) => {
-      this.spinnerService.hide()
-      this.notificationService.sendMessage({message: error.error.message, type:NotificationType.error})
-    })
+      this.spinnerService.hide();
+      this.notificationService.sendMessage({message: error.error.message, type: NotificationType.error});
+    });
     // window.location.reload();
   }
 
-  removeRoleToUser(user_id: string, event:any) {
-    this.spinnerService.show()
-    const role : roleDTO = {
-      role : (<any>RolePayload)[event]
-    }
+  removeRoleToUser(user_id: string, event: any) {
+    this.spinnerService.show();
+    const role: roleDTO = {
+      role : (RolePayload as any)[event]
+    };
     this.adminPagesService.removeRole(user_id, role).subscribe((res: any) => {
-  this.spinnerService.hide()
+    this.spinnerService.hide();
     }, (error: any) => {
-      this.spinnerService.hide()
-      this.notificationService.sendMessage({message: error.error.message, type:NotificationType.error})
-    })
+      this.spinnerService.hide();
+      this.notificationService.sendMessage({message: error.error.message, type: NotificationType.error});
+    });
     // window.location.reload();
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
 
-  baseUrl: 'http://localhost:8000/api/public/auth',
-  baseUrlPro: 'http://localhost:8000/api/protected/',
-  baseUrlPub: 'http://localhost:8000/api/public/',
+  // baseUrl: 'http://localhost:8001/api/public/auth',
+  // baseUrlPro: 'http://localhost:8001/api/protected/',
+  // baseUrlPub: 'http://localhost:8001/api/public/',
 
 
-  // baseUrl: 'http://192.168.1.23:8000/api/public/auth',
-  // baseUrlPro: 'http://192.168.1.23:8000/api/protected/',
-  // baseUrlPub: 'http://192.168.1.23:8000/api/public/',
+  baseUrl: 'http://192.168.8.104:8000/api/public/auth',
+  baseUrlPro: 'http://192.168.8.104:8000/api/protected/',
+  baseUrlPub: 'http://192.168.8.104:8000/api/public/',
 
 
 };
